2a62acb014063ecf1137814c455a390cafb75213
LeoMazzolaDev <leonardogmazzola@gmail.com>
2025-01-09 14:05:08 -0300

    Ajuste de erros e instalação do GCC

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+   float numero1, numero2, resultado;
+   int operador;
+
+   printf("Digite o primeiro número: ");
+   scanf("%f", &numero1);
+
+   printf("Digite o segundo número: ");
+   scanf("%f", &numero2);
+
+   
+   return 0;
+}
