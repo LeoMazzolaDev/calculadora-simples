3a20b029a602b924620dd0300da83387e25aa2e5
LeoMazzolaDev <leonardogmazzola@gmail.com>
2025-01-09 17:30:48 -0300

    versão 1.0 completa

--- main.c
+++ main.c
@@ -1,17 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main()
+{
+   float number1, number2, result;
+   int operation;
 
-int main() {
-   float numero1, numero2, resultado;
-   int operador;
+   printf("Enter the first number: ");
+   scanf("%f", &number1);
 
-   printf("Digite o primeiro número: ");
-   scanf("%f", &numero1);
+   printf("Enter the second number: ");
+   scanf("%f", &number2);
 
-   printf("Digite o segundo número: ");
-   scanf("%f", &numero2);
+   do
+   {
+      printf("What operation do you want to perform?\n");
+      printf("1 - Addition\n");
+      printf("2 - Subtraction\n");
+      printf("3 - Multiplication\n");
+      printf("4 - Division\n");
+      scanf("%d", &operation);
 
-   
+      if (operation <= 0 || operation > 4)
+      {
+         printf("Enter a valid value.\n");
+      }
+   } while (operation <= 0 || operation > 4);
+
+   switch (operation)
+   {
+   case 1:
+      result = number1 + number2; 
+      break;
+
+   case 2:
+      result = number1 - number2;
+      break;
+
+   case 3:
+      result = number1 * number2;
+      break;
+
+   case 4:
+      result = number1 / number2;
+      break;
+
+   default:
+      break;
+   }
+
+   printf("Result: %.2f\n", result);
    return 0;
 }
